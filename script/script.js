c1e45717e7f6297fda47d5d43f79e6672bc09086
Peziiim <eduardoorx@gmail.com>
2025-04-26 16:01:42 -0300

    feat: added menu mobile

--- script/script.js
+++ script/script.js
@@ -1,5 +1,23 @@
 var btnHome = document.querySelector("#headerbtn");
+var menu = document.querySelector(".mobile-menu")
+var nav = document.querySelector(".link-container")
 
 btnHome.addEventListener("click", function (){
     location.reload();
+})
+
+menu.addEventListener("click", () => {
+    if(nav.classList.contains("on")){
+        nav.style.animation = "out 1s ease 1 forwards"
+
+        setTimeout(() => {
+            nav.classList.remove("on");
+               nav.style.animation = "none"
+        }, 1000)
+    } else {
+        nav.classList.add("on")
+        nav.style.animation = "fadeIn 1s ease 1 forwards";
+    }
+
+  
 })
