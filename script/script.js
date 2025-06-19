c921a7ac265cb5510e423da0d41ed47e2c767e5c
Peziiim <eduardoorx@gmail.com>
2025-06-19 16:26:10 -0300

    feat: We Script

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-
 var ponteiro = document.getElementById("ponteiro");
 var card = document.querySelector(".header-card");
 var x = document.getElementById("close-card");
@@ -14,15 +13,23 @@ const title = document.getElementById("QA-title")
 const QaForm = document.getElementById("QA-form")
 //QA
 
+//WE
+ const weIcon = document.getElementById("We-icon")
+ const weMain = document.getElementById("We-main")
+ const weImg = document.getElementById("We-figure")
+ const weContainer = document.getElementById("We-container")
+//WE
+
 let respostas = [
     "-check-in: 14h \n\n-check-out: 11h\n\nO horário de check-in na pousada tem início às 14h, permitindo que a equipe prepare cuidadosamente os quartos para a chegada dos hóspedes. Já o check-out deve ser realizado até às 11h, garantindo tempo adequado para a limpeza e organização dos espaços antes da próxima hospedagem.", 
     "Sim! 🐾 Aceitamos pets com muito carinho. \n\nSabemos como eles são parte da família, por isso, sua companhia é bem-vinda em nossa pousada.",
-    "Atualmente, contamos com 3 quartos confortáveis, todos com capacidade para até 3 pessoas.\n\nCada quarto é equipado com ar-condicionado, Wi-Fi, banheiro privativo e ambiente acolhedor para garantir o seu conforto.",
+    "Atualmente, contamos com 3 quartos confortáveis, todos com capacidade para até 3 pessoas.\n\nCada quarto é equipado, Wi-Fi, banheiro privativo e ambiente acolhedor para garantir o seu conforto.",
     "Sim! Nosso café da manhã está incluso na hospedagem e é servido com muito carinho para começar bem o seu dia.\n\nOferecemos uma seleção simples e caseira com bolos variados, pães, manteiga, café, leite, achocolatado e iogurte. Tudo é preparado com aquele jeitinho acolhedor de interior"
 
 ]
 
-let count = 0;
+let countQA = 0;
+let countWe = 0;
 
 
 Array.from(buttons).forEach((button, i) => {
@@ -44,8 +51,8 @@ Array.from(buttons).forEach((button, i) => {
         title.textContent = button.textContent
         title.style.fontSize = "18px"
         
-        count = 1
-        console.log(count)
+        countQA = 1
+        console.log(countQA)
 
     })
 });
@@ -62,7 +69,7 @@ QA.addEventListener("click", () => {
 
 })
     removeBtn.addEventListener("click", () => {
-      if(count == 0) {
+      if(countQA == 0) {
         QAcont.classList.add("remove")
     
            setTimeout(() => {
@@ -78,11 +85,10 @@ QA.addEventListener("click", () => {
         text.innerHTML = " "
         title.textContent = "Perguntas frequentes"
         title.style.fontSize = ""
-        count = 0
+        countQA = 0
 
       }
     })
-
 
 QaForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -106,6 +112,93 @@ QaForm.addEventListener("submit", (event) => {
     }
          
 });
+
+weIcon.addEventListener("click", () => {
+    weIconClick()
+
+})
+
+function showInfo(json){
+    let figure
+    let date = new Date().getDay()
+    let day
+
+    switch(date){
+        case 1:
+            day = "Domingo"
+            break
+        case 2:
+            day = "Segunda-feira"
+            break
+        case 3:
+            day = "Terça-feira"
+            break
+        case 4:
+            day = "Quarta-feira"
+            break
+        case 5:
+            day = "Quinta-feira"
+            break
+        case 6:
+            day = "Sexta-feira"
+            break
+        case 7:
+            day = "Sabado"
+            break
+    }
+    json.weather = "Rain"
+    
+    if(json.weather === "Clear"){figure = `<img id="We-figure" src="./assets/images/sol.png" alt="Sol">`} else 
+
+    if(json.weather === "Rain"){figure = `<img id="We-figure" src="./assets/images/chuva.png" alt="chuva">`} else 
+    
+    if(json.weather === "Clouds"){figure = `<img id="We-figure" src="./assets/images/nublado.png" alt="nublado">`} else {
+
+        figure =  `<img id="We-figure" src="./assets/images/nublado.png" alt="nublado">`
+    }
+
+    console.log(json.descricao)
+    
+    weMain.innerHTML = `<p id="We-desc">${json.descricao}</p>
+                            ${figure}
+                        <p id="We-temp">${json.temp}°C</p>
+                        <p id="day"> ${day}</p>`  
+
+                        weIcon.style.display = "none"
+                        weContainer.classList.add("resize")
+                        setTimeout(() => {
+                            weMain.style.display = "block"
+                            
+                        }, 700)                 
+}
+
+async function weIconClick(){
+    const apiKey = "8a60b2de14f7a17c7a11706b2cfcd87c"
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=-22.760926479772152&lon=-46.16399932208938&appid=8a60b2de14f7a17c7a11706b2cfcd87c&units=metric&lang=pt_br`
+
+    const response = await fetch(api)
+    const json = await response.json()
+
+    if(json.cod === 200){
+        showInfo({
+            weather: json.weather[0].main,
+            descricao: json.weather[0].description,
+            temp: json.main.temp
+
+        })
+    } else {swal("Erro na requisição da Api", `Erro: cod${json.cod}\n${json.message}`, "warning")}
+
+
+
+}
+
+
+weMain.addEventListener("click", () => {
+    console.log("oi")
+    weMain.style.display = "none"
+    weContainer.classList.add("unsize")
+    weIcon.style.display = "flex"              
+})
 
 ponteiro.addEventListener("click", () => {
     ponteiro.classList.toggle('jump');
